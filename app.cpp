eccb94d9031469c9827c99bfd86df334c17e6955
Amir <amirh4liyan@gmail.com>
2023-11-03 23:09:14 +0330

    finish :)

--- app.cpp
+++ app.cpp
@@ -1,9 +1,12 @@
 #include <cmath>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+
+	cout << "Greetings and good times, Dr.Afshari Rad\n\n";
 
 	double lg = pow(10, -3);;
 	double li = 79.9 * pow(10, -2);
@@ -38,16 +41,36 @@ int main() {
 
 	double* p = new double[11];
 	double* o = new double[11];
+	double* fih = new double[11];
+	double* negative_o = new double[11];
 
-	//double coefficient_fi = 225 * M_PI * pow(10, -4); was similar to Agi
-	//double coefficient_F = 79.9 * pow(10, -2); was similar to li
+	ofstream MyFile("data_set.dat");
+	MyFile << "\"Foolad\"\n";
 	double fi[] = {0, 0.1, 0.32, 0.6, 0.9, 1.08, 1.18, 1.27, 1.33, 1.36, 1.4};
 	double F[] = {0, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2020};
 	for (int i = 0; i < 11; i++) {
 		p[i] = fi[i] * Agi;
 		o[i] = F[i] * li;
+		fih[i] = F[i] * lg;
+		negative_o[i] = -o[i];
 		cout << "fi = " << fi[i] << " --> " << p[i] << "    F = " << F[i] << " --> " << o[i] << endl;
+		MyFile << o[i] << ", " << p[i] << "\n";
 	}
+	MyFile << "\n\n";
+
+	MyFile << "\"Foolad2\"\n";
+	for (int i = 0; i < 11; i++) {
+		MyFile << negative_o[i] << ", " << p[i] << "\n";
+	}
+	MyFile << "\n\n";
+
+	MyFile << "\"Foolad3\"\n";
+	for (int i = 0; i < 11; i++) {
+		MyFile << fih[i]+1200 << ", " << p[i] << "\n";
+	}
+
+	// Close the file
+	MyFile.close();
 
 	return 0;
 }
